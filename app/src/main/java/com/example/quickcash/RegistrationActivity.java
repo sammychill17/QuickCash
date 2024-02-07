921a6517481925bdaaef842945d5c6627d3f6839
James <gaultois.james@gmail.com>
2024-02-07 19:10:19 -0400

    Information being grabbed now

--- app/src/main/java/com/example/quickcash/RegistrationActivity.java
+++ app/src/main/java/com/example/quickcash/RegistrationActivity.java
@@ -3,13 +3,62 @@ package com.example.quickcash;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 
-public class RegistrationActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        Button registerButton = findViewById(R.id.submitButton);
+        registerButton.setOnClickListener(this);
+    }
+
+    protected String getEmailAddress(){
+        EditText emailForm = findViewById(R.id.emailText);
+        return emailForm.getText().toString().trim();
+    }
+
+    protected String getName(){
+        EditText nameForm = findViewById(R.id.nameText);
+        return nameForm.getText().toString().trim();
+    }
+
+    protected String getPassword(){
+        EditText passForm = findViewById(R.id.passText);
+        return passForm.getText().toString().trim();
+    }
+
+    protected String getRole(){
+        RadioButton employeeButton = findViewById(R.id.employeeRole);
+        RadioButton employerButton = findViewById(R.id.employerRole);
+        if(employerButton.isChecked()){
+            return "Employer";
+        }
+        else if(employeeButton.isChecked()){
+            return "Employee";
+        }
+        else{
+            return "";
+        }
+    }
+
+    @Override
+    public void onClick(View view){
+        String email = getEmailAddress();
+        String name = getName();
+        String password = getPassword();
+        String role = getRole();
+
+        Toast test = Toast.makeText(this, "Email: "+email+" Name: "+name+" Password: "+password+" Role: "+role, Toast.LENGTH_SHORT);
+
+        test.show();
+
     }
 
 }
