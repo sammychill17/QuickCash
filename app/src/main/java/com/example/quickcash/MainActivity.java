070ad17b78094d5faa814bf28a2bc24634a4e882
pmorrison <pr581958@dal.ca>
2024-02-06 21:29:23 -0400

    Welcome and Login UI

--- app/src/main/java/com/example/quickcash/MainActivity.java
+++ app/src/main/java/com/example/quickcash/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.quickcash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginbutton = (Button) findViewById(R.id.buttonLogin);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }
