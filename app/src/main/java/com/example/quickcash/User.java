f4717e0e84c00d51154fcda1c22cd52be09976d4
Sam <fz338338@dal.ca>
2024-02-07 21:21:52 -0400

    2 engineering tasks completed for database structure

---
+++ app/src/main/java/com/example/quickcash/User.java
@@ -0,0 +1,61 @@
+package com.example.quickcash;
+
+public class User {
+    private String email;
+    private String password;
+    private String name;
+    private String role;
+
+    public User() {
+        this.email = "";
+        this.password = "";
+        this.name = "";
+        this.role = "";
+    }
+    /*
+    constructor
+     */
+    public User(String email, String password, String name, String role) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.role = role;
+    }
+    /*
+    getters
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    /*
+    Setters
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
