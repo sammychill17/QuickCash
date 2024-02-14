e174a4f8ff87b54f29e57ef268e0bd059526027c
Ali Amini <al935400@dal.ca>
2024-02-13 21:51:21 -0400

    test --implemented edge cases for latitude test

--- app/src/test/java/com/example/quickcash/ExampleUnitTest.java
+++ app/src/test/java/com/example/quickcash/ExampleUnitTest.java
@@ -16,14 +16,27 @@ import static org.junit.Assert.*;
     public void latitudeIsValid() {
         assertTrue(LocationUtil.isValidLatitude("34.0522"));
         assertTrue(LocationUtil.isValidLatitude("-34.0522"));
+    }
+
+    @Test
+    public void latitudeIsInvalid(){
         assertFalse(LocationUtil.isValidLatitude("invalid"));
-        assertFalse(LocationUtil.isValidLatitude("95"));
+        assertFalse("Should have returned false when input is " +
+                        "greater than maximum allowed value for latitude",
+                        LocationUtil.isValidLatitude("90.1"));
+        assertFalse("Should have returned false when input is " +
+                "less than minimum allowed value for latitude",
+                LocationUtil.isValidLatitude("-90.1"));
     }
 
     @Test
     public void longitudeIsValid() {
         assertTrue(LocationUtil.isValidLongitude("-118.2437"));
         assertTrue(LocationUtil.isValidLongitude("118.2437"));
+    }
+
+    @Test
+    public void longitudeIsInvalid(){
         assertFalse(LocationUtil.isValidLongitude("invalid"));
         assertFalse(LocationUtil.isValidLongitude("-200"));
     }
