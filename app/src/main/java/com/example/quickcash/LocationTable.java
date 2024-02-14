9fcb3b4a32c16ad6c284c908c86d2c060f519d70
Sam <fz338338@dal.ca>
2024-02-14 00:25:48 -0400

    implementation -- added event listener for location data received and retrieving location data from database

--- app/src/main/java/com/example/quickcash/LocationTable.java
+++ app/src/main/java/com/example/quickcash/LocationTable.java
@@ -1,7 +1,12 @@
 package com.example.quickcash;
 
+import android.location.Location;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LocationTable {
     private DatabaseReference databaseReference;
@@ -10,7 +15,29 @@ public class LocationTable {
         databaseReference = FirebaseDatabase.getInstance().getReference("Locations");
     }
 
-    public void addLocationToDatabase(location loc) {
+    public void addLocationToDatabase(com.example.quickcash.Location loc) {
         databaseReference.push().setValue(loc);
+    }
+
+    public void retrieveLocationFromDatabase(final OnLocationDataReceivedListener listener) {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot locationSnapshot : dataSnapshot.getChildren()) {
+                    Location loc = locationSnapshot.getValue(Location.class);
+                    listener.onLocationDataReceived(loc);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public interface OnLocationDataReceivedListener {
+        void onLocationDataReceived(Location location);
     }
 }
