4bbc5ff85d9bf6b4135bad14b426b257016a8829
kasuga <lamkashing20040401@gmail.com>
2024-02-17 04:48:48 +0800

    [Implemtation] Implemented logout button functionality

--- app/src/main/java/com/example/quickcash/Constants.java
+++ app/src/main/java/com/example/quickcash/Constants.java
@@ -2,5 +2,5 @@ package com.example.quickcash;
 
 public class Constants {
     static String firebaseUrl = "https://quickcash-6941c-default-rtdb.firebaseio.com/";
-    static String sessionData_spID = "session_login";
+    public static String sessionData_spID = "session_login";
 }
--- app/src/main/java/com/example/quickcash/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/quickcash/ui/notifications/NotificationsFragment.java
@@ -1,6 +1,8 @@
 package com.example.quickcash.ui.notifications;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.quickcash.Constants;
 import com.example.quickcash.MainActivity;
 import com.example.quickcash.databinding.FragmentNotificationsBinding;
 import com.google.android.material.snackbar.Snackbar;
@@ -49,6 +52,10 @@ public class NotificationsFragment extends Fragment {
     }
 
     private void handleLogout() {
+        Context context = requireContext().getApplicationContext();
+        SharedPreferences sp = context.getSharedPreferences(Constants.sessionData_spID, Context.MODE_PRIVATE);
+        sp.edit().clear().apply();
+
         Intent intent = new Intent(requireContext().getApplicationContext(), MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
