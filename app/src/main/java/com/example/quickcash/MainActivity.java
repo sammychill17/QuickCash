9e4e5155006fa2e3b90feb639902893cbddb3b62
Parker Morrison <pr581958@dal.ca>
2024-02-16 09:44:59 -0400

    SharedPreferences login in MainActivity

--- app/src/main/java/com/example/quickcash/MainActivity.java
+++ app/src/main/java/com/example/quickcash/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.quickcash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences sp = getApplicationContext().getSharedPreferences(
+                Constants.sessionData_spID, Context.MODE_PRIVATE);
+
         Button loginbutton = (Button) findViewById(R.id.buttonLogin);
         loginbutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -22,5 +27,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(loginIntent);
             }
         });
+
+        String spEmail = sp.getString("email","");
+        String spPassword = sp.getString("password","");
+
+        // check if user has SP values on app start; if so, attempt to log them in
+        if (!spEmail.equals("") && !spPassword.equals("")) {
+            LoginHandler spLoginHandler = new LoginHandler(spEmail, spPassword,
+                    getApplicationContext(), loginbutton);
+            spLoginHandler.handleLogin();
+        } else {
+            // SP is empty, bring the user to login screen
+            Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(loginIntent);
+        }
     }
 }
