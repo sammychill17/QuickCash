1e18afc20d1a2f4a0454ac07d3ae1422f71e2b3d
kasuga <lamkashing20040401@gmail.com>
2024-02-17 04:15:02 +0800

    [Implemtation] Removed duplicate strings in strings.xml that I did not catch at the merge and added imports for classes that need them (or the code would not compile)

--- app/src/main/java/com/example/quickcash/LoginActivity.java
+++ app/src/main/java/com/example/quickcash/LoginActivity.java
@@ -4,10 +4,13 @@ import static android.app.PendingIntent.getActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
