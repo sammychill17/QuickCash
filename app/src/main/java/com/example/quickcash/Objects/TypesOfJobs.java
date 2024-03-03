867d7199c50a6e70f561af601bcae3422ce34c65
Sam <fz338338@dal.ca>
2024-03-03 00:14:44 -0400

    Implementation -- Created TypesOfJobs Enum

---
+++ app/src/main/java/com/example/quickcash/Objects/TypesOfJobs.java
@@ -0,0 +1,14 @@
+package com.example.quickcash.Objects;
+
+/*
+joke template for enum -- TypesOfJobs
+ */
+public enum TypesOfJobs {
+        HITMAN,
+        YARD_WORK,
+        POLITICIAN_HITMAN,
+        JOB_TITLE,
+        MONK,
+        WAIFU;
+}
+
