000c95bea6c85673482cf8a8a6b09c9b8f186cd5
Sam <fz338338@dal.ca>
2024-03-03 00:13:21 -0400

    Implementation -- created Employer Class

---
+++ app/src/main/java/com/example/quickcash/Objects/Employer.java
@@ -0,0 +1,7 @@
+package com.example.quickcash.Objects;
+
+public class Employer extends User {
+    public Employer(String email, String password, String name, String role) {
+        super(email, password, name, role);
+    }
+}
