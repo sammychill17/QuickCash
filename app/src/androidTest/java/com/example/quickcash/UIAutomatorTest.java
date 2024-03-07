3797d248d29af70810879a7d833bd762c23e8a72
Shreya Rai <sh219628@dal.ca>
2024-03-07 12:13:13 -0400

    [Testing] Created 2 UIAutomator tests to search for job title

--- app/src/androidTest/java/com/example/quickcash/UIAutomatorTest.java
+++ app/src/androidTest/java/com/example/quickcash/UIAutomatorTest.java
@@ -11,6 +11,7 @@ import android.content.SharedPreferences;
 import android.os.Build;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.test.core.app.ApplicationProvider;
@@ -211,7 +212,62 @@ public class UIAutomatorTest  {
         UiObject searchBar = device.findObject(new UiSelector().textContains("Search"));
         assertTrue(searchBar.exists());
     }
-
+    @Test
+    public void jobSearchTestWithTitle() throws UiObjectNotFoundException, InterruptedException {
+        UiObject loginButton = device.findObject(new UiSelector().textContains("LOG IN"));
+        loginButton.clickAndWaitForNewWindow(LAUNCH_TIMEOUT);
+        UiObject loginLabel = device.findObject(new UiSelector().textContains("Log in!"));
+        assertTrue(loginLabel.exists());
+        UiObject passwordBox = device.findObject(new UiSelector().textContains("Password"));
+        assertTrue(passwordBox.exists());
+        passwordBox.setText("iamspiderman");
+        UiObject emailIDBox = device.findObject(new UiSelector().textContains("Email"));
+        assertTrue(emailIDBox.exists());
+        emailIDBox.setText("peterparker22@outlook.com");
+        UiObject anotherLoginButton = device.findObject(new UiSelector().className(Button.class));
+        assertTrue(anotherLoginButton.exists());
+        anotherLoginButton.clickAndWaitForNewWindow(LAUNCH_TIMEOUT);
+        allowPermissionsIfNeeded();
+        UiObject dashboardPage = device.findObject(new UiSelector().textContains("Dashboard"));
+        assertTrue(dashboardPage.exists());
+        UiObject makeMoneyButton = device.findObject(new UiSelector().textContains("Make Money"));
+        assertTrue(makeMoneyButton.exists());
+        makeMoneyButton.clickAndWaitForNewWindow(LAUNCH_TIMEOUT);
+        UiObject searchBar = device.findObject(new UiSelector().textContains("Search"));
+        assertTrue(searchBar.exists());
+        searchBar.setText("Please God, help me fix my marriage");
+        UiObject searchResult = device.findObject(new UiSelector().textContains("Please God, help me fix my marriage").className(TextView.class));
+        assertTrue(searchResult.exists());
+    }
+    @Test
+    public void jobSearchTestWithTitle2() throws UiObjectNotFoundException, InterruptedException {
+        UiObject loginButton = device.findObject(new UiSelector().textContains("LOG IN"));
+        loginButton.clickAndWaitForNewWindow(LAUNCH_TIMEOUT);
+        UiObject loginLabel = device.findObject(new UiSelector().textContains("Log in!"));
+        assertTrue(loginLabel.exists());
+        UiObject passwordBox = device.findObject(new UiSelector().textContains("Password"));
+        assertTrue(passwordBox.exists());
+        passwordBox.setText("iamspiderman");
+        UiObject emailIDBox = device.findObject(new UiSelector().textContains("Email"));
+        assertTrue(emailIDBox.exists());
+        emailIDBox.setText("peterparker22@outlook.com");
+        UiObject anotherLoginButton = device.findObject(new UiSelector().className(Button.class));
+        assertTrue(anotherLoginButton.exists());
+        anotherLoginButton.clickAndWaitForNewWindow(LAUNCH_TIMEOUT);
+        allowPermissionsIfNeeded();
+        UiObject dashboardPage = device.findObject(new UiSelector().textContains("Dashboard"));
+        assertTrue(dashboardPage.exists());
+        UiObject makeMoneyButton = device.findObject(new UiSelector().textContains("Make Money"));
+        assertTrue(makeMoneyButton.exists());
+        makeMoneyButton.clickAndWaitForNewWindow(LAUNCH_TIMEOUT);
+        UiObject searchBar = device.findObject(new UiSelector().textContains("Search"));
+        assertTrue(searchBar.exists());
+        searchBar.setText("Please God, help me fix my marriage");
+        UiObject searchResult = device.findObject(new UiSelector().textContains("Agent 69 for Special Reasons").className(TextView.class));
+        assertFalse(searchResult.exists());
+        UiObject searchResultCorrect = device.findObject(new UiSelector().textContains("Please God, help me fix my marriage").className(TextView.class));
+        assertTrue(searchResultCorrect.exists());
+    }
 
     /*
     clicks the "While using the app" button of the location permissions system prompt
