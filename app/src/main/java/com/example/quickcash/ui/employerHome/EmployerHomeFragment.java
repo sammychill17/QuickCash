f8334a7428a2ff1ae54dabb33c0e77bdddc69ef5
Sam <fz338338@dal.ca>
2024-03-09 11:32:55 -0400

    Implemented -- Major changes made to employer home fragment

--- app/src/main/java/com/example/quickcash/ui/employerHome/EmployerHomeFragment.java
+++ app/src/main/java/com/example/quickcash/ui/employerHome/EmployerHomeFragment.java
@@ -6,18 +6,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.quickcash.Objects.Job;
+import com.example.quickcash.Objects.JobTypes;
 import com.example.quickcash.R;
 import com.example.quickcash.databinding.FragmentEmployerhomeBinding;
 import com.example.quickcash.ui.employerJobList.EmployerJobListFragment;
 import com.example.quickcash.ui.employerJobPost.EmployerJobPostFragment;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Locale;
 
 public class EmployerHomeFragment extends Fragment {
 
@@ -38,26 +49,17 @@ public class EmployerHomeFragment extends Fragment {
 
         final ImageButton jobPostButton = binding.loseYourselfButton;
 
-        jobPostButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-                fragmentTransaction.setReorderingAllowed(true);
-                fragmentTransaction.replace(R.id.dashboardCardView, EmployerJobPostFragment.class, null);
-                fragmentTransaction.commit();
-            }
+        jobPostButton.setOnClickListener(v -> {
+            NavController navController = NavHostFragment.findNavController(EmployerHomeFragment.this);
+            navController.navigate(R.id.employerJobPostFragment);
         });
+
 
         final Button myJobsButton = binding.myJobsButton;
 
-        myJobsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-                fragmentTransaction.setReorderingAllowed(true);
-                fragmentTransaction.replace(R.id.dashboardCardView, EmployerJobListFragment.class, null);
-                fragmentTransaction.commit();
-            }
+        myJobsButton.setOnClickListener(v -> {
+            NavController navController = NavHostFragment.findNavController(EmployerHomeFragment.this);
+            navController.navigate(R.id.employerJobListFragment);
         });
 
         return root;
