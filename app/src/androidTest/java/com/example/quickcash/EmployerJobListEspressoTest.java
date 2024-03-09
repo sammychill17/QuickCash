6346bf967617ddfd4b1589045bd8ee0f4301166c
Sam <fz338338@dal.ca>
2024-03-09 05:46:45 -0400

    Test -- Job list employer espresso test

--- app/src/androidTest/java/com/example/quickcash/EmployerJobListEspressoTest.java
+++ app/src/androidTest/java/com/example/quickcash/EmployerJobListEspressoTest.java
@@ -1,2 +1,46 @@
-package com.example.quickcash;public class EmployerJobListEspressoTest {
+package com.example.quickcash;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import com.example.quickcash.Activities.MainActivity;
+import com.example.quickcash.R;
+import com.example.quickcash.ui.employerJobList.EmployerJobListFragment;
+import com.example.quickcash.ui.employerJobPost.EmployerJobPostFragment;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class EmployerJobListEspressoTest {
+
+    @Before
+    public void launchFragment() {
+        ActivityScenario<MainActivity> activityScenario = ActivityScenario.launch(MainActivity.class);
+        activityScenario.onActivity(activity -> {
+            EmployerJobListFragment employerJobListFragment = new EmployerJobListFragment();
+            activity.getSupportFragmentManager().beginTransaction().add(R.id.dashboardCardView, employerJobListFragment).commit();
+        });
+    }
+
+    @Test
+    public void testMyJobsButton() {
+        onView(withId(R.id.editTextTextEmailAddress)).perform(typeText("parker@morrison.com"));
+        onView(withId(R.id.editTextTextPassword)).perform(typeText("password"));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.buttonGotoLogin)).perform(click());
+        onView(withId(R.id.myJobsButton)).perform(click());
+    }
+
 }
