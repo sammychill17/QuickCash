c5d5f528cfa99596208a621643810114ce757f50
James <gaultois.james@gmail.com>
2024-03-11 19:30:57 -0300

    Testing - Fixed EmployerJobListEspressoTest.java testing framework.

--- app/src/androidTest/java/com/example/quickcash/EmployerJobListEspressoTest.java
+++ app/src/androidTest/java/com/example/quickcash/EmployerJobListEspressoTest.java
@@ -5,21 +5,30 @@ import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
 
+import com.example.quickcash.Activities.DashboardActivity;
 import com.example.quickcash.Activities.MainActivity;
 import com.example.quickcash.R;
 import com.example.quickcash.ui.employerJobList.EmployerJobListFragment;
 import com.example.quickcash.ui.employerJobPost.EmployerJobPostFragment;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -29,18 +38,30 @@ public class EmployerJobListEspressoTest {
     public void launchFragment() {
         ActivityScenario<MainActivity> activityScenario = ActivityScenario.launch(MainActivity.class);
         activityScenario.onActivity(activity -> {
-            EmployerJobListFragment employerJobListFragment = new EmployerJobListFragment();
-            activity.getSupportFragmentManager().beginTransaction().add(R.id.dashboardCardView, employerJobListFragment).commit();
+            InstrumentationRegistry.getInstrumentation().getUiAutomation()
+                    .grantRuntimePermission(activity.getPackageName(), "android.permission.ACCESS_COARSE_LOCATION");
+            InstrumentationRegistry.getInstrumentation().getUiAutomation()
+                    .grantRuntimePermission(activity.getPackageName(), "android.permission.ACCESS_FINE_LOCATION");
+
         });
     }
 
     @Test
-    public void testMyJobsButton() {
+    public void testMyJobsButton() throws UiObjectNotFoundException {
+        onView(withId(R.id.buttonGotoLogin)).perform(click());
         onView(withId(R.id.editTextTextEmailAddress)).perform(typeText("parker@morrison.com"));
         onView(withId(R.id.editTextTextPassword)).perform(typeText("password"));
         Espresso.closeSoftKeyboard();
         onView(withId(R.id.buttonGotoLogin)).perform(click());
         onView(withId(R.id.myJobsButton)).perform(click());
+    }
+
+    private void allowPermissionsIfNeeded() throws UiObjectNotFoundException {
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        UiObject allowPermissions = device.findObject(new UiSelector().text("While using the app"));
+        if (allowPermissions.exists()) {
+            allowPermissions.click();
+        }
     }
 
 }
