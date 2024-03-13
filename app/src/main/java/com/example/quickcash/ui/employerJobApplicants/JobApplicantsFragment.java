474b61a30f21eb5050058ad01cf3d87eb7ee3336
pmorrison <pr581958@dal.ca>
2024-03-12 22:31:24 -0300

    final commit before getApplicantsList() is actually working; laying groundwork for applicants list

--- app/src/main/java/com/example/quickcash/ui/employerJobApplicants/JobApplicantsFragment.java
+++ app/src/main/java/com/example/quickcash/ui/employerJobApplicants/JobApplicantsFragment.java
@@ -1,2 +1,68 @@
-package com.example.quickcash.ui.employerJobApplicants;public class JobApplicantsFragment {
+package com.example.quickcash.ui.employerJobApplicants;
+
+import static androidx.core.content.ContentProviderCompat.requireContext;
+import static androidx.test.InstrumentationRegistry.getContext;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.quickcash.FirebaseStuff.JobDBHelper;
+import com.example.quickcash.Objects.Job;
+import com.example.quickcash.Objects.UserLocation;
+import com.example.quickcash.R;
+import com.example.quickcash.databinding.FragmentEmployerjobapplicantsBinding;
+import com.example.quickcash.databinding.FragmentEmployerjoblistBinding;
+import com.example.quickcash.ui.employerJobList.EmployerJobListAdapter;
+import com.example.quickcash.ui.employerJobApplicants.JobApplicantsViewModel;
+import com.google.firebase.database.DatabaseError;
+
+import java.util.List;
+
+public class JobApplicantsFragment extends Fragment {
+
+    RecyclerView mRecyclerView;
+    EmployerJobListAdapter myAdapter;
+    private FragmentEmployerjobapplicantsBinding binding;
+
+    SharedPreferences sp;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        JobApplicantsViewModel jobApplicantsViewModel =
+                new ViewModelProvider(this).get(JobApplicantsViewModel.class);
+
+        binding = FragmentEmployerjobapplicantsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        Context c = getContext();
+        sp = c.getApplicationContext().getSharedPreferences("session_login", c.MODE_PRIVATE);
+
+        return root;
+    }
+
+    private void getApplicantsList(){
+//        JobDBHelper helper = new JobDBHelper();
+//        //helper.getApplicantsByKey(, new JobDBHelper.ApplicantsObjectCallback() {
+//            @Override
+//            public void onJobsReceived(List<Job> jobs) {
+//                myAdapter.updateJobs(jobs);
+//                myAdapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onError(DatabaseError error) {
+//                Toast.makeText(getContext(), getResources().getString(R.string.DATABASE_REGISTRATION_ERROR) + error.toString(), Toast.LENGTH_LONG).show();
+//            }
+//        });
+    }
 }
---
+++ app/src/main/java/com/example/quickcash/ui/employerJobApplicants/JobApplicantsHolder.java
@@ -0,0 +1,25 @@
+package com.example.quickcash.ui.employerJobApplicants;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.quickcash.R;
+
+public class JobApplicantsHolder extends RecyclerView.ViewHolder{
+    static TextView name;
+    static ImageView imageView;
+    static Button button;
+
+    public JobApplicantsHolder(@NonNull View itemView){
+        super(itemView);
+
+        this.name = itemView.findViewById(R.id.nameJa);
+        this.imageView = itemView.findViewById(R.id.imageJa);
+        this.button = itemView.findViewById(R.id.buttonJa);
+    }
+}
