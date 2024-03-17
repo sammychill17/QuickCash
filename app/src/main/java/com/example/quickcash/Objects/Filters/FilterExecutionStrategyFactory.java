2f6a8d1921f30468c8e4c04a57ddd5cee991b224
kasuga <lamkashing20040401@gmail.com>
2024-03-18 05:29:36 +0800

    [Refactor] Added comments for the inner working classes for search & filter

--- app/src/main/java/com/example/quickcash/Objects/Filters/FilterExecutionStrategyFactory.java
+++ app/src/main/java/com/example/quickcash/Objects/Filters/FilterExecutionStrategyFactory.java
@@ -2,22 +2,55 @@ package com.example.quickcash.Objects.Filters;
 
 import java.util.List;
 
+/**
+ * A factory for instantiating the appropriate strategy for a given set of filters
+ */
 public class FilterExecutionStrategyFactory {
     private List<IFilter> filters;
+    /**
+     * The constant MAX_UNCHAINED_QUERIES_SIZE.
+     * If you set more filters than this it will default to the client-side loop strategy because you will be querying too much
+     * anyway.
+     */
     public static final int MAX_UNCHAINED_QUERIES_SIZE = 3;
 
+    /**
+     * Instantiates a new Filter execution strategy factory.
+     */
     public FilterExecutionStrategyFactory() {}
+
+    /**
+     * Instantiates a new Filter execution strategy factory with te set of filters you wish to use.
+     *
+     * @param filters the filters you wish to use
+     */
     public FilterExecutionStrategyFactory(List<IFilter> filters) {
         this.filters = filters;
     }
 
+    /**
+     * Gets the list of currently active filters.
+     *
+     * @return the list of currently active filters
+     */
     public List<IFilter> getFilters() {
         return filters;
     }
+
+    /**
+     * Sets the list of currently active filters.
+     *
+     * @param filters the list of currently active filters
+     */
     public void setFilters(List<IFilter> filters) {
         this.filters = filters;
     }
 
+    /**
+     * Gets the preferred execution strategy, derived from the list of filters you put in.
+     *
+     * @return the preferred execution strategy
+     */
     public FilterExecutionStrategy getPreferredExecutionStrategy() {
         boolean shouldForceLoopStrategy = false;
         for (IFilter filter : filters) {
