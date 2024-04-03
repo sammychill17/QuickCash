e3f8dee4fbad5986e0f0b3f34abe847208fecd5b
Sam <fz338338@dal.ca>
2024-04-03 04:31:23 -0300

    Implementation -- Button related changes

--- app/src/main/java/com/example/quickcash/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/quickcash/ui/dashboard/DashboardFragment.java
@@ -7,16 +7,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.test.espresso.matcher.ViewMatchers;
 
 import com.example.quickcash.Activities.MyEmployeesActivity;
 import com.example.quickcash.Activities.MyMoneyActivity;
 import com.example.quickcash.R;
 import com.example.quickcash.databinding.FragmentDashboardBinding;
+import com.example.quickcash.ui.employeeHome.EmployeeHomeFragment;
+import com.example.quickcash.ui.employeeJobHistory.EmployeeRatingsHistoryFragment;
 
 public class DashboardFragment extends Fragment {
 
@@ -29,6 +34,7 @@ public class DashboardFragment extends Fragment {
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
 
         SharedPreferences sp = requireContext().getSharedPreferences(getResources().getString(R.string.sessionData_spID), Context.MODE_PRIVATE);
         if (sp.contains("role") && ("Employer".equals(sp.getString("role", "error!")))) {
@@ -47,16 +53,16 @@ public class DashboardFragment extends Fragment {
             binding.myMoneyButton.setOnClickListener(null);
         } else {
             // I am an Employee
-            binding.title.setText("My Rating");
-            binding.meFragmentButton.setImageResource(R.drawable.icon_comic_1);
-            View.OnClickListener onClickListener = new View.OnClickListener() {
+            binding.meFragmentButton.setImageResource(R.drawable.ratingbutton);
+
+            binding.meFragmentButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    //
+                    NavController navController = NavHostFragment.findNavController(DashboardFragment.this);
+                    navController.navigate(R.id.employeeRatingsHistoryFragment);
                 }
-            };
-            binding.meFragmentButton.setOnClickListener(onClickListener);
-            binding.title.setOnClickListener(onClickListener);
+            });
+
             binding.employeeHistoryCardView.setVisibility(View.VISIBLE);
             binding.myMoneyButton.setOnClickListener(new View.OnClickListener() {
                 @Override
