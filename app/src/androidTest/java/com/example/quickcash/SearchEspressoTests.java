a691a76cc31fdcac301090942fb810097754aaf8
Shreya Rai <sh219628@dal.ca>
2024-04-04 14:39:48 -0300

    [Refactoring] Search Espresso Tests refactored

--- app/src/androidTest/java/com/example/quickcash/SearchEspressoTests.java
+++ app/src/androidTest/java/com/example/quickcash/SearchEspressoTests.java
@@ -1,24 +1,31 @@
 package com.example.quickcash;
 
+import static android.view.KeyEvent.KEYCODE_ENTER;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressKey;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.doesNotHaveFocus;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.is;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.assertFalse;
 
+
+import android.widget.TextView;
 
 import com.example.quickcash.Activities.RegistrationActivity;
 import com.example.quickcash.Activities.SearchActivity;
@@ -44,34 +51,21 @@ public class SearchEspressoTests {
     }
 
     @Test
-    public void checkOuiJobTitle() {
+    public void checkTutoringJobTitle() throws InterruptedException{
         String textToType = "Teach me Design Patterns";
         onView(withId(R.id.searchBar)).perform(typeText(textToType));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.jobItem_title)).check(matches(withText(textToType)));
+        Thread.sleep(1000);
+        onView(allOf(withId(R.id.jobItem_title), withText(textToType))).check(matches(isDisplayed()));
     }
 
     @Test
-    public void checkHitmanJobTitle() {
-        String textToType = "Walk my animals";
-        onView(withId(R.id.searchBar)).perform(typeText(textToType));
-        Espresso.closeSoftKeyboard();
-        onView(withId(R.id.jobItem_title)).check(matches(withText(textToType)));
-    }
-
-    @Test
-    public void checkOuiJobTitleComplement() {
+    public void checkTutoringJobTitleComplement() throws InterruptedException{
         String textToType = "Teach me Design Patterns";
         onView(withId(R.id.searchBar)).perform(typeText(textToType));
         Espresso.closeSoftKeyboard();
+        Thread.sleep(1000);
         onView(withText("Walk my animals")).check(doesNotExist());
     }
 
-    @Test
-    public void checkHitmanJobTitleComplement() {
-        String textToType = "Walk my animals";
-        onView(withId(R.id.searchBar)).perform(typeText(textToType));
-        Espresso.closeSoftKeyboard();
-        onView(withText("Teach me Design Patterns")).check(doesNotExist());
-    }
 }
